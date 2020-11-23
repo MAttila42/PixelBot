2bef41aa8f06aabd48761d96a4fe9349f510074c
ExAtom <65223119+ExAtom@users.noreply.github.com>
2020-11-23 18:38:23 +0100

    XP System

---
+++ Commands/Dev/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Discord.WebSocket;
+
+namespace PixelBot.Commands.Dev
+{
+    public class Test
+    {
+        public static string[] Aliases()
+        {
+            string[] aliases =
+            {
+                "test",
+                "teszt"
+            };
+            return aliases;
+        }
+        public static bool HasPerm(SocketMessage message)
+        {
+            bool hasPerm = false;
+            foreach (var i in (message.Author as SocketGuildUser).Roles)
+                if (BaseConfig.GetConfig().Roles.Admin.Contains(i.Id))
+                {
+                    hasPerm = true;
+                    break;
+                }
+            return hasPerm;
+        }
+
+        public static async void DoCommand(SocketMessage message)
+        {
+            await message.Channel.SendMessageAsync("Válasz");
+            await Program.Log("command", message);
+        }
+    }
+}
---
+++ Json/Member.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace PixelBot.Json
+{
+    public class Member
+    {
+        public ulong ID { get; set; }
+        public int XP { get; set; }
+
+        public static List<Member> PullData()
+        {
+            try { return JsonSerializer.Deserialize<List<Member>>(File.ReadAllText("Members.json")); }
+            catch (Exception) { File.WriteAllText("Members.json", "[]"); }
+            return JsonSerializer.Deserialize<List<Member>>(File.ReadAllText("Members.json"));
+        }
+        public static void PushData(List<Member> list)
+        {
+            File.WriteAllText("Members.json", JsonSerializer.Serialize(list, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        public Member() { } // Ez a sztupid Json deserialize miatt kell. Használd a másik konstruktort!
+
+        public Member(ulong id) // Alap, egyszerű konstruktor, egy ID-t megadsz, és létrehozza a saját objektumát a JSON-ben, üres adatokkal.
+        {
+            ID = id;
+            XP = 0;
+        }
+        public Member(ulong id, int xp) // Direkt az XP feljegyzésére létrehozott konstruktor. Kezdeti XP értéket kap.
+        {
+            ID = id;
+            XP = xp;
+        }
+    }
+}
